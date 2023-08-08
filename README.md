5fe2de91a3e0f566b12311a72a44cbcf730e7805
vcjuliocesar <vcjuliocesar@outlook.com>
2023-08-08 12:31:18 -0600

    add:fastapi

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ puthon3 main.py
 # App Project
 
 ```sh
-git clone
+git clone 
 cd app
 python3 -m venv env
 source env/bin/activate
---
+++ web-server/main.py
@@ -0,0 +1,21 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4,5]
+
+@app.get('/contact',response_class=HTMLResponse)
+def get_contact():
+    return """
+        <h1>Hola</h1>
+    """
+
+def run():
+    store.get_categories()
+    
+if __name__ == '__main__':
+    run()
